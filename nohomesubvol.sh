f1ee18eafd29bcf46be32e5ccea4a2db2dec2d6f
mapleroyal <56464571+mapleroyal@users.noreply.github.com>
2023-04-13 17:42:19 +0300

    Update nohomesubvol.sh

--- nohomesubvol.sh
+++ nohomesubvol.sh
@@ -1,23 +1,19 @@
-# STILL A WIP AS OF 12 APR 2023 21:14
-
 #!/bin/bash
 set -e
 
-# first move to root
-echo "Changing directory to root..."
-cd /
+# Steps 1 and 2 can be done as part of a post-install script, because a reboot is required one way or another before you can do step 4.
 
 # 1. Copy user1's home directory
-echo "Copying user1's home directory..."
-rsync -a --info=progress2 /home/user1 /user1
+# echo "Copying user1's home directory..."
+# rsync -a --info=progress2 /home/user1 /user1
+
+# 3. Comment out the line related to /home and @home in /etc/fstab
+# echo "Commenting out /home and @home in /etc/fstab..."
+# sudo sed -i '/^\s*[^#].*\s\/home\s.*@home/s/^/#/' /etc/fstab
 
 # 2. Delete Btrfs subvolume with ID 257
 echo "Deleting Btrfs subvolume with ID 257..."
 sudo btrfs subvolume delete -i 257 /
-
-# 3. Comment out the line related to /home and @home in /etc/fstab
-echo "Commenting out /home and @home in /etc/fstab..."
-sudo sed -i '/^\s*[^#].*\s\/home\s.*@home/s/^/#/' /etc/fstab
 
 # 4. Move /user1 to /home/user1
 echo "Moving /user1 to /home/user1..."
