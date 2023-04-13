3b1ea3fd350498685018c572898ec0f46d66b9e8
mapleroyal <56464571+mapleroyal@users.noreply.github.com>
2023-04-13 19:18:42 +0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -6,9 +6,29 @@ set -e
 echo "Enabling parallel downloads"
 sudo sed -i '/^#Parallel/s/^#//' /etc/pacman.conf
 
-echo "Installing spice-vdagent if not already installed"
+#!/bin/bash
+echo "Checking if spice-vdagent and rsync are installed"
+
+# Install packages if not already installed
+packages_to_install=()
 if ! pacman -Qs spice-vdagent > /dev/null; then
-sudo pacman -S spice-vdagent --noconfirm
+  packages_to_install+=("spice-vdagent")
+else
+  echo "spice-vdagent is already installed"
+fi
+
+if ! pacman -Qs rsync > /dev/null; then
+  packages_to_install+=("rsync")
+else
+  echo "rsync is already installed"
+fi
+
+# If there are packages to install, install them
+if [ "${#packages_to_install[@]}" -gt 0 ]; then
+  echo "Installing packages: ${packages_to_install[*]}"
+  sudo pacman -S "${packages_to_install[@]}" --noconfirm
+else
+  echo "All packages are already installed"
 fi
 
 echo "Removing subvol ID's from /etc/fstab"
