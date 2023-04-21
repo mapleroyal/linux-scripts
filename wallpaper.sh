cc3306bed28378cab7c3d38b784f6a8063a3889e
mapleroyal <56464571+mapleroyal@users.noreply.github.com>
2023-04-21 17:44:25 +0300

    qol updates to wallpaper script

--- wallpaper.sh
+++ wallpaper.sh
@@ -32,54 +32,74 @@ while true; do
         # get current wallpaper path
         current_bg=$( /usr/bin/gsettings get org.gnome.desktop.background picture-uri-dark | tr -d "'" )
         
-        while true; do
-            echo "Choose an option:"
-            echo "1 - Add rounded corners"
-            echo "2 - Darken the image"
-            echo "3 - Both"
-            echo "q - Quit"
-            
-            mod_option=$(dd bs=1 count=1 2>/dev/null)
-            
-            if [[ $mod_option == "q" ]]; then
-                exit
-            fi
-            
-            rounded_corners="/home/$USER/Pictures/wallpapers/roundedcorners.png"
-            
-            # get current wallpaper path
-            current_bg=$( /usr/bin/gsettings get org.gnome.desktop.background picture-uri-dark | tr -d "'" )
-            
-            # create a temporary image
-            temp_image="/tmp/temp_bg.png"
-            cp "$current_bg" "$temp_image"
-            
-            # create a directory to store the modified image
-            mkdir -p "/home/$USER/Pictures/wallpapers/current"
-            timestamp=$(date +%Y%m%d_%H%M%S)
-            output_image="/home/$USER/Pictures/wallpapers/current/currentbg_$timestamp.jpg"
-            
-            # Remove the oldest file in the directory, leaving only the most recent one
-            find "/home/$USER/Pictures/wallpapers/current" -type f -iname "currentbg_*" -printf "%T@ %p\n" | sort -n | head -n -1 | cut -d' ' -f 2- | xargs rm -f
-            
-            # options 1 or 3
-            if [[ $mod_option == "1" || $mod_option == "3" ]]; then
-                # add rounded corners
-                convert "$temp_image" \( "$rounded_corners" -resize "$(identify -format "%wx%h" "$temp_image")" \) -gravity center -composite "$temp_image"
-            fi
-            
-            # options 2 or 3
-            if [[ $mod_option == "2" || $mod_option == "3" ]]; then
-                # darken the image
-                convert "$temp_image" -fill black -colorize 60% "$temp_image"
-            fi
-            # copy the modified image to the output location
-            cp "$temp_image" "$output_image"
-            
-            # set wallpaper with
-            /usr/bin/gsettings set org.gnome.desktop.background picture-uri-dark "$output_image"
+        echo "Choose modifications (type numbers without spaces and press Enter):"
+        echo "1 - Add rounded corners"
+        echo "2 - Darken the image"
+        echo "3 - Blur the image"
+        echo "4 - Pixelate the image"
+        echo "q - Quit"
+        
+        # Reset terminal settings for modification input
+        stty sane
+        
+        read -r mod_options
+        
+        # Restore terminal settings
+        stty -icanon -echo min 1 time 0
+        
+        if [[ $mod_options == "q" ]]; then
+            exit
+        fi
+        
+        rounded_corners="/home/$USER/Pictures/wallpapers/roundedcorners.png"
+        
+        # get current wallpaper path
+        current_bg=$( /usr/bin/gsettings get org.gnome.desktop.background picture-uri-dark | tr -d "'" )
+        
+        # create a temporary image
+        temp_image="/tmp/temp_bg.png"
+        cp "$current_bg" "$temp_image"
+        
+        # create a directory to store the modified image
+        mkdir -p "/home/$USER/Pictures/wallpapers/current"
+        timestamp=$(date +%Y%m%d_%H%M%S)
+        output_image="/home/$USER/Pictures/wallpapers/current/currentbg_$timestamp.jpg"
+        
+        # Remove the oldest file in the directory, leaving only the most recent one
+        find "/home/$USER/Pictures/wallpapers/current" -type f -iname "currentbg_*" -printf "%T@ %p\n" | sort -n | head -n -1 | cut -d' ' -f 2- | xargs rm -f
+        
+        for mod_option in $(echo $mod_options | grep -o .); do
+            case $mod_option in
+                1)
+                    # add rounded corners
+                    convert "$temp_image" \( "$rounded_corners" -resize "$(identify -format "%wx%h" "$temp_image")" \) -gravity center -composite "$temp_image"
+                ;;
+                2)
+                    # darken the image
+                    convert "$temp_image" -fill black -colorize 60% "$temp_image"
+                ;;
+                3)
+                    # blur the image
+                    convert "$temp_image" -blur 0x43 "$temp_image"
+                ;;
+                4)
+                    # pixelate the image
+                    convert "$temp_image" -scale 5% -scale 2000% "$temp_image"
+                ;;
+                *)
+                    echo "Invalid input. Please try again."
+                ;;
+            esac
         done
+        
+        # copy the modified image to the output location
+        cp "$temp_image" "$output_image"
+        
+        # set wallpaper with
+        /usr/bin/gsettings set org.gnome.desktop.background picture-uri-dark "$output_image"
+        
     else
         echo "Invalid input. Please try again."
     fi
+    
 done
