7abe2424680204eaf7d80710ebe0cc1f266ee5f3
name <email>
2023-05-07 18:24:43 +0000

    use kitty instead now

--- runthewallpaperscript-gnome.sh
+++ runthewallpaperscript-gnome.sh
@@ -4,4 +4,4 @@
 wallpaper_script_path="/home/user1/scripts/wallpaper-gnome.sh"
 
 # Launch the interactive wallpaper script in a new kgx terminal
-kgx --title "Wallpaper Script" sh -c "$wallpaper_script_path; read -n1 -p 'Press any key to exit...'"
+kitty sh -c "$wallpaper_script_path; read -n1 -p 'Press any key to exit...'"
